70b2d8c89eda7696950aa0930e97b33f2e2f0c73
constantyn-silvian <costidimian@gmail.com>
2026-03-05 00:58:48 +0200

    Added it on github pages

--- bianegrila/vite.config.js
+++ bianegrila/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bianegrila/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
 })
